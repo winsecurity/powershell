126a1e24f23455dfe43ab709a242848dd84aa5f8
nikhil-polymath <nagasainikhil100@gmail.com>
2020-11-27 09:52:52 +0530

    Add files via upload

---
+++ monitor.ps1
@@ -0,0 +1,32 @@
+﻿
+param($filepath,$process_name)
+
+Try
+{
+Get-Process -Name $process_name -ErrorAction Stop
+write "The process is already running"
+}
+
+Catch
+{
+write "Starting the process"
+Start-Process $filepath
+}
+
+
+while($true)
+{
+
+Try
+{
+Get-Process -Name $process_name -ErrorAction Stop
+}
+
+Catch
+{
+Start-Process $filepath
+}
+
+Start-Sleep -Seconds 5
+
+}
